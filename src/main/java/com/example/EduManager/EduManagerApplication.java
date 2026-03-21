bd725684a1c2e664ab2370ce7c0dc8f6e3044c81
Jun-77 <ghkdqhdmswns@naver.com>
2026-03-21 15:40:30 +0900

    first commit

---
+++ src/main/java/com/example/EduManager/EduManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EduManager/EduManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
