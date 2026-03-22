c49c0cee618c28ac8e19a10dfa3a6c1506bd0124
Jun-77 <ghkdqhdmswns@naver.com>
2026-03-22 20:30:59 +0900

    fix: 배포 오류 수정

--- src/test/java/com/example/EduManager/EduManagerApplicationTests.java
+++ src/test/java/com/example/EduManager/EduManagerApplicationTests.java
@@ -1,13 +1,11 @@
 package com.example.EduManager;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EduManagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
